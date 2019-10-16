8c8541fa26f7f89f78c01113d9176302a9f1ba64
varunvaruns9 <varunsinghs2021@gmail.com>
2019-10-16 11:29:00 +0530

    Add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class EssentialOil(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Through(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    oil = models.ForeignKey(EssentialOil, on_delete=models.CASCADE)
