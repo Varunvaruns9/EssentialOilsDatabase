e9ec2675ba8dacf05f81e1f6057642d7aafffcbc
varunvaruns9 <varunsinghs2021@gmail.com>
2019-10-16 11:52:06 +0530

    Add admin and models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Plant, EssentialOil, Through
 
-# Register your models here.
+
+admin.site.site_header = "Essential Oils Database"
+admin.site.site_title = "Essential Oils Database"
+admin.site.register(Plant)
+admin.site.register(EssentialOil)
+admin.site.register(Through)
--- app/models.py
+++ app/models.py
@@ -3,12 +3,23 @@ from django.db import models
 
 class Plant(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 
 class EssentialOil(models.Model):
     name = models.CharField(max_length=200)
+    abbr = models.CharField(max_length=10)
+    sname = models.CharField(max_length=200)
+    family = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Through(models.Model):
     plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
     oil = models.ForeignKey(EssentialOil, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.plant.name + ' - ' + self.oil.name
