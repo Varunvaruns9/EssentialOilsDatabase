f73b819332401ed13a3a1ae0cad2713b9a49e4a3
varunvaruns9 <varunsinghs2021@gmail.com>
2019-10-22 19:07:47 +0530

    Edit database schemas

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Plant, EssentialOil, Through
+from .models import Metabolite, EssentialOil, Through
 
 
 admin.site.site_header = "Essential Oils Database"
 admin.site.site_title = "Essential Oils Database"
-admin.site.register(Plant)
+admin.site.register(Metabolite)
 admin.site.register(EssentialOil)
 admin.site.register(Through)
--- app/models.py
+++ app/models.py
@@ -1,25 +1,36 @@
 from django.db import models
-
-
-class Plant(models.Model):
-    name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class EssentialOil(models.Model):
     name = models.CharField(max_length=200)
     abbr = models.CharField(max_length=10)
-    sname = models.CharField(max_length=200)
+    scientific_name = models.CharField(max_length=200)
     family = models.CharField(max_length=100)
+    graph = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Metabolite(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Through(models.Model):
-    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    metabolite = models.ForeignKey(Metabolite, on_delete=models.CASCADE)
     oil = models.ForeignKey(EssentialOil, on_delete=models.CASCADE)
+    identity = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(100), ])
+    mzratio = models.FloatField()
+    time = models.FloatField(validators=[MinValueValidator(0), ])
+    relative_abundance1 = models.FloatField()
+    relative_abundance2 = models.FloatField()
+    relative_abundance3 = models.FloatField()
+    relative_abundance4 = models.FloatField()
 
     def __str__(self):
-        return self.plant.name + ' - ' + self.oil.name
+        return self.oil.name + ' - ' + self.metabolite.name
